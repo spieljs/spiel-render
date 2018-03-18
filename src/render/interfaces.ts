b0173d2b7b27ed0ae36cb749598b4989edd1b4d9
dominuskernel <promodominus@gmail.com>
2018-03-18 02:42:14 +0100

    allow to write the types for state argument

--- src/render/interfaces.ts
+++ src/render/interfaces.ts
@@ -3,6 +3,6 @@ import {VNode} from "ultradom";
 export type Keys = string;
 export type State = {[k in Keys]: any};
 
-export type View = (state: State) => VNode<any>;
+export type View = (state: any) => VNode<any>;
 export type JSXElements = VNode<any>;
 export type Children = Array<VNode<{}>>;
